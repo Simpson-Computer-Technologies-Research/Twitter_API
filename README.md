e976f929b146e0b4974c2f03f3027f0261f1a7d8
Tristan <75189508+realTristan@users.noreply.github.com>
2022-11-11 14:12:18 -0500

    Add files via upload

---
+++ README.md
@@ -0,0 +1,16 @@
+# Twitter Tweeter ![Stars](https://img.shields.io/github/stars/realTristan/TwitterTweeter?color=brightgreen) ![Watchers](https://img.shields.io/github/watchers/realTristan/TwitterTweeter?label=Watchers)
+![jydesign-social-media-header-sizes-1080x630](https://user-images.githubusercontent.com/75189508/186441393-fe2e7605-bb03-4b7f-b3c2-139cc54ee26d.png)
+
+# About
+A Quick Wrapper for sending Tweets
+
+# License
+MIT License
+
+Copyright (c) 2022 Tristan Simpson
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
---
+++ main.py
@@ -0,0 +1,52 @@
+from requests_oauthlib import OAuth1Session
+
+class TwitAuth:
+    def __init__(self, key: str, secret: str):
+        self.session = OAuth1Session(key, client_secret=secret)
+        self.secret = secret
+        self.key = key
+
+    # Get the tokens
+    def fetch_oauth_tokens(self):
+        r = self.session.fetch_request_token(
+            "https://api.twitter.com/oauth/request_token?oauth_callback=oob&x_auth_access_type=write"
+        )
+        print(f"Authoization Required: ", 
+           self.session.authorization_url("https://api.twitter.com/oauth/authorize")
+        )
+        return OAuth1Session(self.key,
+            client_secret=self.secret,
+            resource_owner_key=r.get('oauth_token'),
+            resource_owner_secret=r.get("oauth_token_secret"),
+            verifier=input("Enter Authorization PIN: "),
+        ).fetch_access_token("https://api.twitter.com/oauth/access_token")
+
+
+    # Get final post session
+    def fetch_auth_session(self):
+        tokens = self.fetch_oauth_tokens()
+        return OAuth1Session(self.key,
+            client_secret=self.secret,
+            resource_owner_key=tokens["oauth_token"],
+            resource_owner_secret=tokens["oauth_token_secret"],
+        )
+    
+    # Create a new tweet
+    def create_tweet(self, session: OAuth1Session, message: str):
+        return session.post("https://api.twitter.com/2/tweets", json={"text": message})
+
+
+
+if __name__ == "__main__":
+    twit_auth = TwitAuth(
+        key="YOUR TWITTER API KEY",
+        secret="YOUR TWITTER API SECRET"
+    )
+    session = twit_auth.fetch_auth_session()
+
+    # Create new tweets
+    m1 = twit_auth.create_tweet(session, "Message 1")
+    print(f"{m1.status_code}: {m1.json()}")
+
+    m2 = twit_auth.create_tweet(session, "Message 2")
+    print(f"{m2.status_code}: {m2.json()}")
